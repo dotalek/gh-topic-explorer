f37c35693d9717db8feabb2db75bd87e73528d94
Alejandro Duarte <alekduar@gmail.com>
2022-09-01 21:02:18 -0500

    Resolve main pull conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
+import { useQuery } from "@apollo/client";
+import { GET_TOPICS_BY_NAME } from "./api/githubQueries";
+
+const App = () => {
+  const { loading, error, data } = useQuery(GET_TOPICS_BY_NAME, {
+    variables: { name: "react" },
+  });
+
+  if (loading) return <div className="">Loading Topics...</div>;
+
+  if (error) return <div className="">Error while fetching topics</div>;
+
   return <div className="App"></div>;
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ApolloProvider } from "@apollo/client";
+import GHClient from "./api/github";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={GHClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
 );
