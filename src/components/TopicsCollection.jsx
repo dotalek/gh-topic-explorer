b0c4629707bec12bce415005eb7b4300bc445622
Alejandro Duarte <alekduar@gmail.com>
2022-09-02 14:19:27 -0500

    Improved display and feedback of main and related topics

--- src/components/TopicsCollection.jsx
+++ src/components/TopicsCollection.jsx
@@ -9,36 +9,39 @@ export const TopicsCollection = ({ searchTerm, clickHandler }) => {
   });
 
   // Give some feedback in the component related to the API call
-  if (loading) return <div className="">Loading Topics...</div>;
-  if (error) return <div className="">Error while fetching topics</div>;
+  if (loading) return <h3 className="">Loading Topics...</h3>;
+  if (error) return <h3 className="">Error while fetching topics</h3>;
 
-  // Build the Topics renders
-  let mainTopic;
-  let relatedTopics;
+  let mainTopic = <h3>No main topic was found</h3>;
+  let relatedTopics = <h3>No related topics were found</h3>;
+  // Only build the Topics components if we received data
   if (data.topic) {
-    mainTopic = <Topic key={data.topic.id} topic={data.topic} />;
-    relatedTopics = data.topic.relatedTopics.map((topic) => {
-      return <Topic key={topic.id} topic={topic} clickHandler={clickHandler} />;
-    });
+    mainTopic = (
+      <>
+        <h3>The main topic is:</h3>
+        <Topic key={data.topic.id} topic={data.topic} />
+      </>
+    );
+
+    // Only build related topics if needed
+    if (data.topic.relatedTopics.length) {
+      relatedTopics = (
+        <>
+          <h3>The related topics are;</h3>
+          {data.topic.relatedTopics.map((topic) => {
+            return (
+              <Topic key={topic.id} topic={topic} clickHandler={clickHandler} />
+            );
+          })}
+        </>
+      );
+    }
   }
   return (
-    <section>
-      {mainTopic ? (
-        <>
-          <h3>The main topic is:</h3>
-          {mainTopic}
-        </>
-      ) : (
-        <h3>No main topic was found</h3>
-      )}
-      {relatedTopics ? (
-        <>
-          <h3>The related topics are:</h3>
-          {relatedTopics}
-        </>
-      ) : (
-        <h3>No related topics were found</h3>
-      )}
-    </section>
+    <output>
+      {mainTopic}
+      <hr />
+      {relatedTopics}
+    </output>
   );
 };
