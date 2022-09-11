947f63ba743fcced596be56d6f3316585a15a9d6
Alek <8705181+the-alek@users.noreply.github.com>
2022-09-10 21:48:23 -0500

    Convert Topic to a styled component (#7)
    * Added styled-components to dependencies
    
    * Translated the Topic component to a styled component

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -1,19 +1,49 @@
-import "./Topic.css";
+import styled from "styled-components";
+
+const StyledTopic = styled.article`
+  border-radius: 1rem;
+  padding: 1rem;
+  width: 10rem;
+  outline: 1px solid gray;
+  margin: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+
+  &:hover {
+    background-color: gray;
+  }
+`;
+
+const TopicName = styled.h4`
+  font-size: 1rem;
+  font-weight: bold;
+`;
+
+const Icon = styled.i`
+  font-size: 0.75rem;
+`;
+
+const StarCount = styled.strong`
+  font-size: 0.75rem;
+`;
+
+const Stargazers = styled.span``;
 
 export const Topic = ({ topic, clickHandler }) => {
   const { name, stargazerCount } = topic;
   return (
-    <article
-      className="topic"
+    <StyledTopic
       onClick={() => {
         clickHandler(name);
       }}
     >
-      <h4 className="topic-name">{name}</h4>
-      <span className="topic-stargazercount">
-        <i aria-label={`has ${stargazerCount} stargazers`}>⭐️</i>
-        <strong>{stargazerCount}</strong>
-      </span>
-    </article>
+      <TopicName>{name}</TopicName>
+      <Stargazers>
+        <Icon aria-label={`has ${stargazerCount} stargazers`}>⭐️</Icon>
+        <StarCount>{stargazerCount}</StarCount>
+      </Stargazers>
+    </StyledTopic>
   );
 };
